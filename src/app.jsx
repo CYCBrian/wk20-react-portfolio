f0a6b52262a5b0529fbadf986bf41482ba48231f
CYCBrian <143226784+CYCBrian@users.noreply.github.com>
2024-03-02 20:33:06 -0500

    1. Added file structur. Added dependencies. Added Boilerplates

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import './app.css'
+// import \components\ from \components location\
+
+function App () {
+    return(
+        <section className="portfolio-app">
+            {/* //Components here// */}
+        </section>
+    )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+    plugin:[react()],
+    server:{
+        port: 3000,
+        open: true
+    }
+})
