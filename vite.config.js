29aa2022d56c568b8f074e5227a6265693c7be6d
CYCBrian <143226784+CYCBrian@users.noreply.github.com>
2024-03-03 20:39:34 -0500

    2.Redid file structure

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugin:[react()],
-    server:{
-        port: 3000,
-        open: true
-    }
+  plugins: [react()],
 })
